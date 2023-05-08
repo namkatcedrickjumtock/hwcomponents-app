a6fecb83a90dd16691b4c0e74c6aff14ad0c6ceb
Namkat Cedrick Jumtock <namkatcedrickjumtock3@gmail.com>
2023-05-08 17:35:03 +0100

    initial commit with skeleton app setup📍

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      {/* Stack the columns on mobile by making one full-width and the other half-width */ }
+      <Row>
+        <Col xs={ 12 } md={ 8 }>
+          xs=12 md=8
+        </Col>
+        <Col xs={ 6 } md={ 4 }>
+          xs=6 md=4
+        </Col>
+      </Row>
+
+    </Container>
   );
 }
+
 
 export default App;
