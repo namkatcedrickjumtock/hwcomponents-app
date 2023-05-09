029cb552d20eec7dfdb9ef166901e9cc7716c86f
Namkat Cedrick Jumtock <namkatcedrickjumtock3@gmail.com>
2023-05-09 15:17:59 +0100

    complete design and responsiveness.

--- src/App.js
+++ src/App.js
@@ -5,29 +5,32 @@ import Col from 'react-bootstrap/Col'
 import PreSignInScreen from './components/PreSignInScreen';
 import Header from './components/Header';
 import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 
 function App() {
+  const [isButtonDisable, setDisable] = useState(true)
+  const [userType, setUserType] = useState("Create Account")
   return (
-    <Container fluid>
+    <Container className='wrapper'>
       {/* header component */ }
       <Header />
 
       {/* main container */ }
-      <Container className='border rounded w-50 my-5 p-5'>
-        <h3 className='text-center p-3 '>Join as a client or freelancer</h3>
-        <Row>
-          <Col xs={ 12 } md={ 6 } >
+      <Container className='my-4   main-container'>
+        <h3 className='text-center py-4 fs-4 '>Join as a client or freelancer</h3>
+        <Row className='d-flex justify-content-around p-4'>
+          <Col xs={ 12 } md={6 }  >
             <PreSignInScreen cardText={ `
-                I’m a client, hiring for a project`} img={ './icons/icon2.png' } />
+                I’m a client, hiring for a project`} img={ './icons/icon2.png' } setUserType={ setUserType } userType="employer" setDisable={ setDisable } />
           </Col>
-          <Col xs={ 12 } md={ 6 }>
-            <PreSignInScreen cardText={ `I’m a freelancer, looking for work` } img="./icons/icon1.png" />
+          <Col xs={ 12 } md={6 } >
+            <PreSignInScreen cardText={ `I’m a freelancer, looking for work` } img="./icons/icon1.png" setDisable={ setDisable } setUserType={ setUserType } userType="freelancer" />
           </Col>
         </Row>
 
-        <div className="p-3">
-          <Button variant="primary" size="md" className='rounded-pill  mb-4 mx-auto d-block'>Join as a Client</Button>
-          <p className='text-sm font-weight-light text-center'>Already have an account? <a href="#"> Log In</a></p>
+        <div className="p-3 btn-container">
+          <Button disabled={ isButtonDisable } size="md" className='rounded-pill text-sm mb-4 mx-auto d-block'>{ userType }</Button>
+          <p className='text-sm font-weight-light text-center'> Already have an account? <a href="#"> Log In</a></p>
         </div>
       </Container>
 
--- src/components/PreSignInScreen.js
+++ src/components/PreSignInScreen.js
@@ -4,10 +4,12 @@ import Form from 'react-bootstrap/Form';
 import './PreSignInScreen.css'
 
 
-function PreSignInScreen({ img, cardText }) {
+function PreSignInScreen({ img, cardText, setDisable, userType, setUserType }) {
     return (
-        // <Card.Link href="#">
-        <Card style={ { width: '15rem' } } >
+        <Card   style={ { width: '13rem' } } className='presigninCards mb-3 m-0' onClick={ () => {
+            setDisable(false)
+            userType !== "employer" ? setUserType("Apply as a Freelancer") : setUserType("Join as a Client")
+        } } >
             <Card.Body>
                 <Card.Title className='d-flex justify-content-md-between'>
                     <img src={ img } alt="" srcset="" />
@@ -15,13 +17,11 @@ function PreSignInScreen({ img, cardText }) {
                         type='radio'
                     />
                 </Card.Title>
-                <Card.Text>
+                <Card.Text className='fs-6 fw-bold'>
                     { cardText }
                 </Card.Text>
             </Card.Body>
         </Card>
-        // </Card.Link>
-
     );
 }
 
