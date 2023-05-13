1ab87fb01897f46acbd3b02a03c644fd4e740972
Namkat Cedrick Jumtock <namkatcedrickjumtock3@gmail.com>
2023-05-13 20:09:49 +0100

    completed log in without responsive.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react'
 import ActivateScreen from './components/Activate';
 import Presigin from "./components/Presigin.js"
 import SignUp from './components/Signup';
+import SignIn from './components/SignIn';
 
 const App = () => {
   return (
     // <Presigin />
     // <SignUp />
-    <ActivateScreen />
+    <SignIn />
   );
 }
 
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -6,38 +6,19 @@ import "../styles/signin.css"
 const SignIn = () => {
     return (
         <div>
-            <Container className=''>
-                <Row className="vh-100 d-flex justify-content-center align-items-end ">
-                    <Col md={ 8 } lg={ 6 } xs={ 12 }>
-                        <Card className="signinCard ">
-                            <h2 className='text-center py-3'>Log in to Upwork</h2>
-                            <Card.Body>
-                                    <div className=" w-75 mx-auto position-relative">
-                                        <Form>
-                                            <Form.Group className="mb-3 " controlId="formGridAddress2">
-                                                {/* <UserIcon className='user-icon' /> */}
-                                                <Form.Control type="email" placeholder="Email" className='shadow-none p-2' />
-                                            </Form.Group>
-                                        </Form>
-                                        <Button className='rounded-pill w-75 mb-4 mx-auto d-block' type="submit">
-                                            Continue with Email
-                                        </Button>
-                                    </div>
-                                    <div className=" w-75 mx-auto">
-                                        <Form>
-                                            <Form.Group className="mb-3 " controlId="formGridAddress2">
-                                                {/* <UserIcon className='user-icon' /> */}
-                                                <Form.Control type="email" placeholder="Email" className='shadow-none p-2' />
-                                            </Form.Group>
-                                        </Form>
-                                        <Button className='rounded-pill w-75 mb-4 mx-auto d-block' type="submit">
-                                            Continue with Email
-                                        </Button>
-                                    </div>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
+            <Container className='vh-100 d-flex align-items-center '>
+                <div className="mx-auto w-50 position-relative">
+                    <h2 className='text-center  fw-bold'>Log in to Upwork</h2>
+                    <Form >
+                        <Form.Group className="mb-3 " controlId="formGridAddress2">
+                            <UserIcon className='user-icon' />
+                            <Form.Control type="email" placeholder="Email" className='shadow-none px-5' />
+                        </Form.Group>
+                    </Form>
+                    <Button className='rounded-pill w-50 mb-4 mx-auto d-block' type="submit">
+                        Continue with Email
+                    </Button>
+                </div>
             </Container>
         </div>
     )
